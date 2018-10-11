2775b180eef54fde300faf6578f615f94c3fb098
02593659203 <02593659203@JIPA-LABIN05-28.ifro.local>
2018-10-11 17:12:51 -0400

    Calculadora

--- Calculadora/src/calculadora/LoginController.java
+++ Calculadora/src/calculadora/LoginController.java
@@ -5,9 +5,17 @@
  */
 package calculadora;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +24,15 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField usuario;
+    @FXML
+    private TextField senha;
+    @FXML
+    private Button btnacessar;
+    @FXML
+    private Button btCadastrar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +40,33 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+    @FXML
+    private void acessar(ActionEvent event) throws IOException {
+       
+                     abrirTela("Calculadora.fxml", "Calculadora", 598, 426);
+
+        
+        
+    }
+ 
+      private void abrirTela(String caminho, String titulo, int wigth, int height) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource(caminho));
+        Scene scene = new Scene(fxmlLoader.load(),wigth,height);
+        Stage stage = new Stage();
+        stage.setTitle(titulo);
+        stage.setScene(scene);
+        stage.show();  
+    } 
+
+    @FXML
+    private void cadastrar(ActionEvent event) {
+        
+        
+    }
+
+   
 }
+
+    
+
